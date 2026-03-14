1d29814c63586f243cd817e041cdb4e255f29808
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 15:28:35 +0530

    fix: modernize code + rewrite README

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -34,7 +34,6 @@ ggplot() +
   ylab('Salary')
 
 # Visualising the Test set results
-library(ggplot2)
 ggplot() +
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
              colour = 'red') +
